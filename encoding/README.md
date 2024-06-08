4de212bd55e5d00a411f4b94cd8e6edfda21a57b
maxym_ko <maxym.koval.cyb@gmail.com>
2024-06-08 16:49:14 +0300

    feat(encoding): implement `grpc-server`

---
+++ encoding/README.md
@@ -0,0 +1,12 @@
+## Run instructions
+
+### 1. Start gRPC Server
+```shell
+cd grpc-server
+mvn clean install
+mvn exec:java -Dexec.mainClass=com.maxym.grpc.GrpcServer 
+```
+
+### 2. Run gRPC Client
+- `cd grpc-client`
+ 
---
+++ encoding/grpc-server/src/main/java/com/maxym/grpc/GrpcServer.java
@@ -0,0 +1,36 @@
+package com.maxym.grpc;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+public class GrpcServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                                     .addService(new MessageServiceImpl())
+                                     .build();
+
+        server.start();
+        System.out.println("Server started, listening on " + 8080);
+
+        server.awaitTermination();
+    }
+
+    static class MessageServiceImpl extends MessageServiceGrpc.MessageServiceImplBase {
+
+        @Override
+        public void sendMessage(MessageRequest request, StreamObserver<MessageResponse> responseObserver) {
+            String message = request.getMessage();
+            String responseMessage = "Received: " + message;
+
+            MessageResponse response = MessageResponse.newBuilder()
+                                                      .setResponse(responseMessage)
+                                                      .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+}
